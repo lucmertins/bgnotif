54a0725054b228ca1fb5969631acc01d3f483e8a
mertins <luciano@mertins.com.br>
2020-03-27 16:45:29 -0300

    Background Notification worked, thanks to developer's answers. https://github.com/MaikuB/flutter_local_notifications/issues/549

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,17 +1,24 @@
 import UIKit
 import Flutter
+import workmanager
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+    static func registerPlugins(with registry: FlutterPluginRegistry){
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
     UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
     if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     }
+    
+    AppDelegate.registerPlugins(with: self)
+    WorkmanagerPlugin.setPluginRegistrantCallback{registry in
+        AppDelegate.registerPlugins(with: registry)
+    }
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
